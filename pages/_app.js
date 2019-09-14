2a11e91ead207a786b1da4466a8090b7351bbf77
Marcelo Dapper <marcelo.dapper@gmail.com>
2019-09-14 14:49:31 -0300

    Enhance themes from @comparae/ui (#4)
    * Upgrade @comparae/ui
    * Fix themes from @comparae/ui

--- pages/_app.js
+++ pages/_app.js
@@ -2,13 +2,13 @@ import App from 'next/app';
 import React from 'react';
 import { ThemeProvider } from 'styled-components';
 
-import { Dark } from '@comparae/ui';
+import { DarkTheme } from '@comparae/ui';
 
 export default class MyApp extends App {
   render() {
     const { Component, pageProps } = this.props;
     return (
-      <ThemeProvider theme={Dark}>
+      <ThemeProvider theme={DarkTheme}>
         <Component {...pageProps} />
       </ThemeProvider>
     );
